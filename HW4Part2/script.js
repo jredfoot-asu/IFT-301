e103546b0acce7be13a1dd4fe63c2e915054031c
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-09-16 09:27:23 -0400

    Homework for week 4 part 2 completed.

---
+++ HW4Part2/script.js
@@ -0,0 +1,7 @@
+let memeName = "First Rule";
+let likeMeme = "it sums up how everyone feels about the year 2020.";
+let yearMeme = 2021;
+let memeSentence = 'The first time I saw "' + memeName + '" was in ' + yearMeme + ' and I liked it because ' + likeMeme;
+
+document.getElementById('title').innerHTML = memeName
+document.getElementById('paragraph1').innerHTML = memeSentence

6a5dd2cd2a012939b9e24d493a8c1ea9b1a3717e
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-09-24 14:10:21 -0400

    Adding week 6 part 1 homework started.

---
+++ HW6Part1/script.js
@@ -0,0 +1,20 @@
+let character1 = document.createElement("h1");
+let image1 = document.createElement("img");
+image1.src = 'superman.jpg'
+character1.innerHTML = 'Superman';
+document.getElementById('card1').appendChild(character1);
+document.getElementById('card1').appendChild(image1)
+
+let character2 = document.createElement("h1");
+let image2 = document.createElement("img");
+image2.src = 'batman.jpeg'
+character2.innerHTML = 'Batman';
+document.getElementById('card2').appendChild(character2);
+document.getElementById('card2').appendChild(image2)
+
+let character3 = document.createElement("h1");
+let image3 = document.createElement("img");
+image3.src = 'wonderwoman.jpeg'
+character3.innerHTML = 'Wonder Woman';
+document.getElementById('card3').appendChild(character3);
+document.getElementById('card3').appendChild(image3)
